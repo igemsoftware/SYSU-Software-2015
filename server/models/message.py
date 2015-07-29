8691ae4eb0c7d210d4b028fbd07c88c3f5b221f8
piscesdream <hapiscesdream@gmail.com>
2015-07-29 19:24:51 +0800

    message

---
+++ server/models/message.py
@@ -0,0 +1,33 @@
+from .. import db
+
+from datetime import datetime
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now)
+    type = db.Column(db.Integer) # reply:0 or lab:1 reminder
+    isread = db.Column(db.Boolean, default=False)
+
+    #sender_id = db.Column(db.Integer)
+    sender_id = db.Column(db.Integer) #, db.ForeignKey('user.id'))
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    title = db.Column(db.String(128))
+    content = db.Column(db.Text)
+
+
+    def __repr__(self):
+        return '<Message[%d]: from %r to %r>' % (self.id, self.sender_id, self.receiver_id)
+    
+
+
+
+#   message_send = db.Table('message_send',
+#       db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+#       db.Column('message_id', db.Integer, db.ForeignKey('message.id')),
+#   )
+#   message_recv = db.Table('message_recv',
+#       db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+#       db.Column('message_id', db.Integer, db.ForeignKey('message.id')),
+#   )
--- tests/test_user.py
+++ tests/test_user.py
@@ -11,7 +11,7 @@ class TestUser(TestCase):
         db.session.commit()
         assert u in db.session
 
-    def test_unique(self):
+    def test_name_unique(self):
         u = User(username='testing', email='foo@example.com', password='123')
         db.session.add(u)
         db.session.commit()
@@ -19,6 +19,14 @@ class TestUser(TestCase):
         from sqlalchemy.exc import IntegrityError
         u = User(username='testing', email='bar@example.com', password='123')
         self.assertRaises(IntegrityError , db.session.add(u))
+
+    def test_name_length(self):
+        u = User(username='i'*300, email='foo@example.com', password='123')
+        db.session.add(u)
+        db.session.commit()
+        
+        assert len(u.username) <= 128
+
 
 
 
