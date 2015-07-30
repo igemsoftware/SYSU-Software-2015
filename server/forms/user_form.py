31d24f4438c07f026bef8b9d6223729463ae46fd
Piscesdream <hapiscesdream@gmail.com>
2015-07-31 00:10:44 +0800

    ideas about part model

--- server/forms/user_form.py
+++ server/forms/user_form.py
@@ -9,53 +9,25 @@ from wtforms import ValidationError
 
 from ..models import User 
 
-class LoginFormWithUsername(Form):
+class LoginForm(Form):
     username = StringField('Username', validators=[Required('Please input your username'), Length(0, 128)])
     password = PasswordField('Password', validators=[Required('Please input your password.'), Length(0, 32)])
-    #remember_me = BooleanField('Keep me logged in')
+    # remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Login')
 
     def validate_username(self, field):
         u = User.query.filter_by(username=field.data).first()
         if not u:
-            raise ValidationError('The user doesn\'t exist.')
-
-
-class LoginForm(Form):
-    email = StringField('Email', validators=[Required('Please input your email.'), Length(1, 64),
-                                             Email('Please input a valid email.')])
-    password = PasswordField('Password', validators=[Required('Please input your password.')])
-    #remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Login')
-
-    def validate_email(self, field):
-        u = User.query.filter_by(email=field.data).first()
-        if not u:
-            raise ValidationError('The email does\'t exist.')
-
-class UE_ChangeForm(Form):
-    username = StringField('Username', validators=[Required('Cannot be empty.'), Length(1, 128, 'Over length')])
-    email = StringField('Email', validators=[Required('Cannot be empty.'), Length(1, 128, 'Over length'),
-                                             Email('Please input a valid email.')])
-    submit = SubmitField('Save')
-
-    def validate_email(self, field):
-        u = User.query.filter_by(email=field.data).first()
-        if u and u != current_user:
-            raise ValidationError('The email is existing.')
-
-    def validate_username(self, field):
-        u = User.query.filter_by(username=field.data).first()
-        if u and u != current_user:
-            raise ValidationError('The username is existing.')
+            raise ValidationError('The username doesn\'t exist.')
 
 class RegistrationForm(Form):
-    email = StringField('Email', validators=[Required('Cannot be empty.'), Length(1, 64),
-                                           Email('Please input a valid email.')])
     username = StringField('Username', validators=[
         Required('Cannot be empty'), Length(1, 64)])#, Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                    #        'Usernames must have only letters, '
                                    #        'numbers, dots or underscores')])
+
+    email = StringField('Email', validators=[Required('Cannot be empty.'), Length(1, 64),
+                                           Email('Please input a valid email.')])
     password = PasswordField('Password', validators=[
         Required('Cannot be emtpy.'), EqualTo('password2', message='Doesn\'t match,')])
     password2 = PasswordField('Reenter password', validators=[Required('Cannot be empty.')])
@@ -70,8 +42,6 @@ class RegistrationForm(Form):
         u = User.query.filter_by(username=field.data).first()
         if u and u != current_user:
             raise ValidationError('The username is existing.')
-
-
 
 class ChangePasswordForm(Form):
     old_password = PasswordField('Old password', validators=[Required('Cannot be empty.')])
@@ -95,6 +65,13 @@ class PasswordResetForm(Form):
             raise ValidationError('The email doesn\'t exist')
 
 
+
+
+
+
+
+
+# unused
 class PasswordResetRequestForm(Form):
     email = StringField('Email', validators=[Required('Cannot be empty'), Length(1, 64),
                                              Email('Invalid email')])
@@ -105,4 +82,33 @@ class PasswordResetRequestForm(Form):
         if u == None:
             raise ValidationError('The email doesn\'t exist')
 
+# unused
+class LoginFormWithEmail(Form): 
+    email = StringField('Email', validators=[Required('Please input your email.'), Length(1, 64),
+                                             Email('Please input a valid email.')])
+    password = PasswordField('Password', validators=[Required('Please input your password.')])
+    #remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Login')
+
+    def validate_email(self, field):
+        u = User.query.filter_by(email=field.data).first()
+        if not u:
+            raise ValidationError('The email does\'t exist.')
+
+# unused
+class UE_ChangeForm(Form):
+    username = StringField('Username', validators=[Required('Cannot be empty.'), Length(1, 128, 'Over length')])
+    email = StringField('Email', validators=[Required('Cannot be empty.'), Length(1, 128, 'Over length'),
+                                             Email('Please input a valid email.')])
+    submit = SubmitField('Save')
+
+    def validate_email(self, field):
+        u = User.query.filter_by(email=field.data).first()
+        if u and u != current_user:
+            raise ValidationError('The email is existing.')
+
+    def validate_username(self, field):
+        u = User.query.filter_by(username=field.data).first()
+        if u and u != current_user:
+            raise ValidationError('The username is existing.')
 
