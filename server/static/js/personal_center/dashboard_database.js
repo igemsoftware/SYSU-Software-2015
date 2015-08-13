5314499e79633b980a880683730f60df3bc48d7a
leasunhy <leasunhy@gmail.com>
2015-08-13 14:03:01 +0800

    personal database milestone

--- server/static/js/personal_center/dashboard_database.js
+++ server/static/js/personal_center/dashboard_database.js
@@ -1,14 +1,45 @@
+var helper = {
+    contains: function(val, search) {
+        if (Vue.util.isPlainObject(val)) {
+            for (var key in val)
+                if (this.contains(val[key], search))
+                    return true;
+        } else if (Vue.util.isArray(val)) {
+            var i = val.length;
+            while (i--)
+                if (this.contains(val[i], search))
+                    return true;
+        } else if (val != null) {
+            return val.toString().toLowerCase().indexOf(search) > -1;
+        }
+    },
+};
+
+
 var vDatabaseComponent = Vue.component('dashboard-database', {
     data    : function() {
         return {
             databaseRecords : [
             {
-                title           : 'DNA and Protein',
+                title           : 'DNA and Protein 1',
                 description     : 'They listened in silence. No one interrupted.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur aut officia necessitatibus possimus neque vitae expedita dolor autem, reprehenderit reiciendis ad esse ab sapiente minus, inventore voluptatem molestias assumenda rerum!',
                 tags            : ['Task', 'Share'],
                 completeness    : 0,
                 time            : 2,
-                author          : 'leasunhy',
+            },
+            {
+                title           : 'DNA and Protein 2',
+                description     : 'They listened in silence. No one interrupted.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur aut officia necessitatibus possimus neque vitae expedita dolor autem, reprehenderit reiciendis ad esse ab sapiente minus, inventore voluptatem molestias assumenda rerum!',
+                tags            : ['Public'],
+                completeness    : 2,
+                time            : 1,
+            },
+            {
+                title           : 'DNA and Protein 3',
+                description     : 'They listened in silence. No one interrupted.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur aut officia necessitatibus possimus neque vitae expedita dolor autem, reprehenderit reiciendis ad esse ab sapiente minus, inventore voluptatem molestias assumenda rerum!',
+                tags            : ['Public'],
+                completeness    : 2,
+                time            : 1,
             },
             {
                 title           : 'DNA and Protein',
@@ -16,7 +47,6 @@ var vDatabaseComponent = Vue.component('dashboard-database', {
                 tags            : ['Public'],
                 completeness    : 2,
                 time            : 1,
-                author          : 'leasunhy',
             },
             {
                 title           : 'DNA and Protein',
@@ -24,7 +54,6 @@ var vDatabaseComponent = Vue.component('dashboard-database', {
                 tags            : ['Public'],
                 completeness    : 2,
                 time            : 1,
-                author          : 'leasunhy',
             },
             {
                 title           : 'DNA and Protein',
@@ -32,23 +61,6 @@ var vDatabaseComponent = Vue.component('dashboard-database', {
                 tags            : ['Public'],
                 completeness    : 2,
                 time            : 1,
-                author          : 'leasunhy',
-            },
-            {
-                title           : 'DNA and Protein',
-                description     : 'They listened in silence. No one interrupted.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur aut officia necessitatibus possimus neque vitae expedita dolor autem, reprehenderit reiciendis ad esse ab sapiente minus, inventore voluptatem molestias assumenda rerum!',
-                tags            : ['Public'],
-                completeness    : 2,
-                time            : 1,
-                author          : 'leasunhy',
-            },
-            {
-                title           : 'DNA and Protein',
-                description     : 'They listened in silence. No one interrupted.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur aut officia necessitatibus possimus neque vitae expedita dolor autem, reprehenderit reiciendis ad esse ab sapiente minus, inventore voluptatem molestias assumenda rerum!',
-                tags            : ['Public'],
-                completeness    : 2,
-                time            : 1,
-                author          : 'leasunhy',
             },
             {
                 title           : 'DNA and Protein',
@@ -56,18 +68,35 @@ var vDatabaseComponent = Vue.component('dashboard-database', {
                 tags            : ['Task'],
                 completeness    : 1,
                 time            : 3,
-                author          : 'leasunhy',
             }
             ],
             currentOrder    : 'time',
             orderReverse    : true,
             orders          : ['time', 'completeness', 'Task', 'Public', 'Share'],
             editStatus      : false,
+            filterData      : null,
+            filterKey       : null,
+            filterRev       : false,
+            filterMine      : true,
+            selectedRecord  : null,
+            selectedTag     : 'all',
         }
     },
     template: '#database-template',
-    created : function() {
+    ready : function() {
         $(function() { $("#order_dropdown").dropdown(); });
+    },
+    methods : {
+        getRecordClass: function(completeness) {
+            return ['design', 'modeling', 'experiment'][completeness];
+        },
+        selectTag: function(tag, mine, data, key, reverse) {
+            this.selectedTag = tag;
+            this.filterMine = mine;
+            this.filterData = data;
+            this.filterKey = key;
+            this.filterRev = reverse;
+        },
     },
     filters : {
         recordOrderBy: function(arr, sortKey, reverse) {
@@ -90,12 +119,16 @@ var vDatabaseComponent = Vue.component('dashboard-database', {
                 if (!aHasTag && bHasTag) return -order;
                 return a.time === b.time ? 0 : a.time > b.time ? order : -order;
             });
-        }
+        },
+        recordFilterBy : function (arr, search, delimiter, dataKey, reverse) {
+            console.log(search);
+            if (search == null)
+                return arr;
+            search = ('' + search).toLowerCase();
+            return arr.filter(function (item) {
+                return reverse ^ (dataKey ? helper.contains(item[dataKey], search) : helper.contains(item, search));
+            });
+        },
     },
-    methods : {
-        getRecordClass: function(completeness) {
-            return ['design', 'modeling', 'experiment'][completeness];
-        }
-    }
 });
 
