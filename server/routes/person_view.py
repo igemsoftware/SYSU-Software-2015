080af81d3283d91c330f26acc6b9bbd056fae585
leasunhy <leasunhy@gmail.com>
2015-08-17 21:03:05 +0800

    use Circuit.jsonify to prepare json to send to client

--- server/routes/person_view.py
+++ server/routes/person_view.py
@@ -16,14 +16,14 @@ def index():
 @login_required
 def mine():
     l = Circuit.query.filter_by(owner=current_user).all()
-    l = map(json_parser, l)
+    l = map(Circuit.jsonify, l)
     return jsonify(mine=l)
 
 @person.route('/favorite')
 @login_required
 def favorite():
     l = current_user.favorite_circuits
-    l = map(json_parser, l)
+    l = map(Circuit.jsonify, l)
     return jsonify(favorite=l)
 
 
