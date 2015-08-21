77fb683ded294f45e34da29c50e667d80c90335d
leasunhy <leasunhy@gmail.com>
2015-08-21 23:51:02 +0800

    taskhall index milestone 2

--- server/static/js/taskhall/taskhall_index.js
+++ server/static/js/taskhall/taskhall_index.js
@@ -3,10 +3,34 @@ Vue.component('taskhall-index', {
     data: function() { return {
         questionCount: 2723,
         selectedTab  : 'question',
+        currentOrder : 'vote',
+        orders       : ['vote', 'view', 'time'],
+        page         : 1,
+        tasks        : [],
     }},
     ready: function() {
+        var store = this;
         $('#taskhall-index-sticky').sticky({
             offset: 60,
         });
+        $('#taskhall-index-order-dropdown').dropdown();
+        this.$eval('updateTasks(this, this.page, this.currentOrder)');
+        this.$watch('page', function() {
+            this.$eval('updateTasks(this, this.page, this.currentOrder)');
+        });
+        this.$watch('currentOrder', function() {
+            this.page = 1;
+            this.$eval('updateTasks(this, this.page, this.currentOrder)');
+        });
+        $('.taskhall-index.question.item > .question.main > .detail').ellipsis();
+    },
+    methods: {
+        updateTasks : function(store, page, currentOrder) {
+            $.get('/taskhall/list?page=' + page + '&keyword=' + currentOrder, function(data) {
+                store.tasks = data.tasks;
+                console.log(data);
+            });
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+        },
     },
 });
