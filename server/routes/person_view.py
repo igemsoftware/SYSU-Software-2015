9cd907681a21f8354b181013d47f78543b677b8e
piscesdream <hapiscesdream@gmail.com>
2015-08-28 19:03:03 +0800

    interface[message/notification]

--- server/routes/person_view.py
+++ server/routes/person_view.py
@@ -24,3 +24,35 @@ def favorite():
     l = map(json_parser, l)
     return jsonify(favorite=l)
 
+
+@person.route('/notifications')
+@login_required
+def get_person_notifications():
+    l = []
+    for msg in current_user.msg_box:
+        l.append({'id': msg.id,
+                  'source': msg.source,
+                  'content': msg.content,
+                  'read': msg.isread})
+    return jsonify(notifications=l)
+
+
+@person.route('/notifications/id', methods=['POST'])
+@login_required
+def mark_notification_as_read(id):
+    m = Message.query.get(id)
+    if m:
+        m.isread = True
+        db.session.add(m)
+
+@person.route('/notifications/id', methods=['DELETE'])
+@login_required
+def delete_notification(id):
+    m = Message.query.get(id)
+    if m:
+        db.session.remove(m)
+
+
+
+
+
