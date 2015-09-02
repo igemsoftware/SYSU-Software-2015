67ba96f644efcfe94e076bc19a203bb758a7b689
leasunhy <leasunhy@gmail.com>
2015-09-03 03:14:11 +0800

    embedded: add icon to edit-in-core

--- server/static/js/embedded.js
+++ server/static/js/embedded.js
@@ -12,16 +12,6 @@ var vBody = new Vue({
             datasets: [
             {
                 label: "My First dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [3, 3, 3, 3, 3]
-            },
-            {
-                label: "My First dataset",
                 fillColor: "rgba(220,220,220,0.2)",
                 strokeColor: "rgba(220,220,220,1)",
                 pointColor: "rgba(220,220,220,1)",
@@ -30,6 +20,16 @@ var vBody = new Vue({
                 pointHighlightStroke: "rgba(220,220,220,1)",
                 data: [3, 5, 4, 2, 4]
             },
+            {
+                label: "My First dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [3, 3, 3, 3, 3]
+            },
             ]
         });
 
@@ -37,7 +37,7 @@ var vBody = new Vue({
         $('#embedded-rating > .rating.container > .rating').rating({
             onRate: function(value) {
                 var criterionIndex = $(this).data('criterion-index');
-                radar.datasets[0].points[criterionIndex].value = value;
+                radar.datasets[1].points[criterionIndex].value = value;
                 radar.update();
             },
         });
