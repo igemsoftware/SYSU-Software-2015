cd12edbd052158d8caeba963d9c664b76ae33ef7
leasunhy <leasunhy@gmail.com>
2015-09-03 02:59:01 +0800

    embedded: rating part

--- server/static/js/embedded.js
+++ server/static/js/embedded.js
@@ -3,5 +3,43 @@ var vBody = new Vue({
     el: "body",
     data: {
         selectedTab: 0,
-    }
+        ratingAspects: ['Safety', 'Feasibility', 'A', 'B', 'C'],
+    },
+    ready: function() {
+        var ctx = document.getElementById("embedded-rating-radar").getContext("2d");
+        var radar = new Chart(ctx).Radar({
+            labels: this.ratingAspects,
+            datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [3, 3, 3, 3, 3]
+            },
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [3, 5, 4, 2, 4]
+            },
+            ]
+        });
+
+        var store = this;
+        $('#embedded-rating > .rating.container > .rating').rating({
+            onRate: function(value) {
+                var aspectIndex = $(this).data('aspect-index');
+                radar.datasets[0].points[aspectIndex].value = value;
+                radar.update();
+            },
+        });
+    },
 });
