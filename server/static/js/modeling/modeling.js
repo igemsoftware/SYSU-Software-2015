2a408137c15a35c94387e4f23034020ea637f493
linjinjin123 <494779656@qq.com>
2015-09-09 00:43:23 +0800

    try chart

--- server/static/js/modeling/modeling.js
+++ server/static/js/modeling/modeling.js
@@ -1,25 +1,49 @@
-$(function () { 
-    $('#map').highcharts({                   //图表展示容器，与div的id保持一致
-        chart: {
-            type: 'column'                         //指定图表的类型，默认是折线图（line）
-        },
+$(function () {
+    $('#map').highcharts({
         title: {
-            text: 'My first Highcharts chart'      //指定图表标题
+            text: 'Monthly Average Temperature',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com',
+            x: -20
         },
         xAxis: {
-            categories: ['my', 'first', 'chart']   //指定x轴分组
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         },
         yAxis: {
             title: {
-                text: 'something'                  //指定y轴的标题
-            }
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
         },
-        series: [{                                 //指定数据列
-            name: 'Jane',                          //数据列名
-            data: [1, 0, 4]                        //数据
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
         }, {
-            name: 'John',
-            data: [5, 7, 3]
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }, {
+            name: 'Berlin',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+        }, {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
         }]
     });
 });
+
+$(".ui.menu .item").tab();
