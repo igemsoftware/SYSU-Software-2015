276c8cf657b28d086addf468eac9374d87cafc26
leasunhy <leasunhy@gmail.com>
2015-09-10 01:38:18 +0800

    PC: notification works

--- server/routes/person_view.py
+++ server/routes/person_view.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from . import person
-from ..models import Design 
+from .. import db
+from ..models import Design, Message
 from ..tools.parser import json_parser
 
 from flask.ext.login import current_user, login_required
@@ -51,13 +52,15 @@ def mark_notification_as_read(id):
     if m:
         m.isread = True
         db.session.add(m)
+    return 'success'
 
 @person.route('/notifications/<int:id>', methods=['DELETE'])
 @login_required
 def delete_notification(id):
     m = Message.query.get(id)
     if m:
-        db.session.remove(m)
+        db.session.delete(m)
+    return 'success'
 
 
 
