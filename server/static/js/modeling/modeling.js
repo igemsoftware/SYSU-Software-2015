e353ae57ae6345d1ad867b3efd884d45db0a31bd
linjinjin123 <494779656@qq.com>
2015-09-09 16:48:22 +0800

    finish demo chart

--- server/static/js/modeling/modeling.js
+++ server/static/js/modeling/modeling.js
@@ -1,49 +1,87 @@
-$(function () {
-    $('#map').highcharts({
-        title: {
-            text: 'Monthly Average Temperature',
-            x: -20 //center
+"use strict"
+
+
+var modeling;
+
+function Modeling() {
+    this.variables = null;
+    this.xAxis = null;
+}
+
+Modeling.prototype.drawChart = function() {
+    var that = this;
+    // for (var i in this.xAxis) {
+    //     this.xAxis[i] =  parseFloat(this.xAxis[i]);
+    // }
+    $('#chart').highcharts({
+        plotOptions: {
+            series: {
+                marker: {
+                    enabled: false, /*数据点是否显示*/
+                },            
+            }
         },
-        subtitle: {
-            text: 'Source: WorldClimate.com',
+        chart: {
+          type: 'spline'
+        },
+        title: {
+            text: 'Dynamic Performance',
             x: -20
         },
+        subtitle: {
+            text: 'I am subtitle',
+            x: -20
+        },
+        marker: {
+                    enabled: false
+                },
         xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            categories: that.xAxis,
+            labels: {
+                    formatter: function() {
+                        return this.value.toFixed(2);//这里是两位小数，你要几位小数就改成几
+                    },
+                },
         },
         yAxis: {
             title: {
-                text: 'Temperature (°C)'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
+                text: 'Output'
+            }
+            // plotLines: [{
+            //     value: 0,
+            //     width: 1,
+            //     color: '#808080'
+            // }]
         },
-        tooltip: {
-            valueSuffix: '°C'
-        },
+        // tooltip: {
+        //     valueSuffix: '°C'
+        // },
         legend: {
             layout: 'vertical',
             align: 'right',
             verticalAlign: 'middle',
             borderWidth: 0
         },
-        series: [{
-            name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-        }, {
-            name: 'New York',
-            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-        }, {
-            name: 'Berlin',
-            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-        }, {
-            name: 'London',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-        }]
+        series: that.variables
+        // series: that.variables
     });
-});
+}
+
+Modeling.prototype.loadData = function() {
+    var that = this;
+    $.get("/modeling/circuit/1", function(data) {
+        console.log(data);
+        that.variables = data.variables;
+        that.xAxis = data.x_axis;
+        console.log(that.variables);
+        console.log(that.xAxis);
+        that.drawChart();
+    });
+}
+
+$(function() {
+    modeling = new Modeling();
+    modeling.loadData();
+})
 
 $(".ui.menu .item").tab();
