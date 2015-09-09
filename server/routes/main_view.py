b72bc265bafa6328317652a17eb49c1e488c654b
leasunhy <leasunhy@gmail.com>
2015-09-09 23:20:39 +0800

    edit main_view to be consistent with master

--- server/routes/main_view.py
+++ server/routes/main_view.py
@@ -4,7 +4,6 @@ from . import main
 
 from flask import render_template, jsonify, request, current_app, url_for, jsonify, abort
 from flask.ext.login import login_required
-from ..models import Circuit
 import json
 
 @main.before_app_request 
@@ -27,27 +26,6 @@ def design():
 @login_required
 def experiment():
     return render_template('experiment.html')
-
-@login_required
-@main.route('/circuit/<int:id>')
-def circuit(id): 
-    # skip: check whether current user has the privilege 
-    
-    c = Circuit.query.get(id)
-    if not c:
-        abort(404)
-
-    return jsonify(
-            title = c.title,
-            protocol = json.loads(c.protocol),
-            )
-
-
-@main.route('/protocol')
-def protocol():
-    protocols = map(lambda x: x.jsonify(), Protocol.query.filter_by(recommend=True).all())
-    return jsonify(protocols=protocols)
-
 
 @main.route('/embedded/<int:id>')
 def embedded(id):
