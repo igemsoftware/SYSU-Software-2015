18574cbef165c5f46d0b04cc341827ecf899b2f9
leasunhy <leasunhy@gmail.com>
2015-09-16 01:31:01 +0800

    embedded tweaks

--- server/static/js/embedded.js
+++ server/static/js/embedded.js
@@ -4,24 +4,27 @@ var vBody = new Vue({
     data: {
         selectedTab: 0,
         ratingCriteria: ['Compatibility', 'Safety', 'Demand', 'Completeness', 'Efficiency', 'Reliability', 'Accessibility'],
+        oriRating: [1, 1, 1, 1, 3, 3, 3],
+        curRating: [3, 3, 3, 3, 3, 3, 3],
+        rated: false,
     },
     ready: function() {
         var ctx = document.getElementById("embedded-rating-radar").getContext("2d");
-        var radar = new Chart(ctx).Radar({
+        radar = new Chart(ctx).Radar({
             labels: this.ratingCriteria,
             datasets: [
             {
-                label: "My First dataset",
+                label: "Original Rating",
                 fillColor: "rgba(220,220,220,0.2)",
                 strokeColor: "rgba(220,220,220,1)",
                 pointColor: "rgba(220,220,220,1)",
                 pointStrokeColor: "#fff",
                 pointHighlightFill: "#fff",
                 pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [3, 5, 4, 2, 4, 2, 4]
+                data: this.oriRating,
             },
             {
-                label: "My First dataset",
+                label: "Your Rating",
                 fillColor: "rgba(151,187,205,0.2)",
                 strokeColor: "rgba(151,187,205,1)",
                 pointColor: "rgba(151,187,205,1)",
@@ -39,7 +42,14 @@ var vBody = new Vue({
                 var criterionIndex = $(this).data('criterion-index');
                 radar.datasets[1].points[criterionIndex].value = value;
                 radar.update();
+                store.curRating[criterionIndex] = value;
             },
+        });
+        this.$watch('oriRating', function() {
+            for (var i = this.ratingCriteria.length - 1; i >= 0; i--) {
+                radar.datasets[0].points[i].value = this.oriRating[i];
+            };
+            radar.update();
         });
     },
 });
