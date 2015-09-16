c80818c5c9c74e06f8b4dfbcdc651869c7343d8c
piscesdream <hapiscesdream@gmail.com>
2015-09-16 13:03:28 +0800

    sidebar

--- server/static/js/bank_detail.js
+++ server/static/js/bank_detail.js
@@ -1,1 +1,17 @@
 var vBody = new Vue({ el: 'body' });
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var x = $(window).scrollTop()+$(window).height()/2 ;
+        var indices = $('#index .list .item');
+        var titles = $('.title')
+  
+        titles.each(function(ind, ele) {
+            if ($(ele).position().top < x) 
+                $(indices[ind]).addClass('active').siblings().removeClass('active')
+                                
+        });
+    });
+
+});
