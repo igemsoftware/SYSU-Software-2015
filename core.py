2de5c74150bd2a7afee3b615865e964db3a78d20
leasunhy <leasunhy@gmail.com>
2015-09-19 11:35:43 +0800

    fix core.py

--- core.py
+++ core.py
@@ -1,4 +1,5 @@
 from server import create_app
+from gevent.wsgi import WSGIServer
 
 # Change the host and port here
 HOST = '0.0.0.0'
@@ -12,6 +13,7 @@ kwargs = {
 
 if __name__ == '__main__':
     #app.run(**kwargs)
+    from gevent import monkey
     monkey.patch_socket()
     server = WSGIServer((HOST, PORT), app)
     server.serve_forever()
